6265b5e49b1c82636f698fc9a4135ed96b6a9399
RalphCuizon <32386939+RalphCuizon@users.noreply.github.com>
2021-08-18 18:57:44 +0200

    design login en registratie activity

--- HooperTracker/app/src/main/java/be/cuizon/hoopertracker/MainActivity.kt
+++ HooperTracker/app/src/main/java/be/cuizon/hoopertracker/MainActivity.kt
@@ -1,11 +1,22 @@
 package be.cuizon.hoopertracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnLoginSignUp.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- documentatie.md
+++ documentatie.md
@@ -4,6 +4,6 @@
 |PHP en mySQL Basics - W3Schools|https://www.w3schools.com/php/php_mysql_intro.asp|
 |login en registratie PHP mySQL|https://www.youtube.com/watch?v=B3R2GNA5zeI&list=PLrUNEklLHiKVMkfpF3G9HVf0AstEeZ6Sm&index=34|
 |error plugin not updated - Android Studio|https://youtrack.jetbrains.com/issue/IDEA-237012?_ga=2.150720821.2017773613.1629300321-1902068659.1629300321|
-|||
+|Volley Library (HTTPS)|https://developer.android.com/training/volley|
 |||
 |||
